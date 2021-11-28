fcc702ef2393133542975afabd73389167920971
w <nnhjgg@163.com>
2021-11-28 15:58:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted } from 'vue';
+import { Viewer } from 'cesium';
+
+onMounted(()=>{
+  const viewer = new Viewer('cesiumContainer');
+})
+</script>
+
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+html,body,#cesiumContainer{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
